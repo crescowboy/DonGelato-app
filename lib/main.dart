f80529791f5fbacb4fdc33b203284cea0e90d3c4
wcrespo <wellingtoncrespo2001@gmail.com>
2023-08-12 23:34:43 -0500

    conectando con firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:don_gelato/pages/login_page.dart';
 import 'package:flutter/material.dart';
+//impotaciones firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-void main() => runApp(MyApp());
+void main() async{ 
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
+  runApp( MyApp());
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
