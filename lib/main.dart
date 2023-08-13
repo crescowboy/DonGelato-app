80e553c93e944898a664a833512f41417541f5dd
wcrespo <wellingtoncrespo2001@gmail.com>
2023-08-12 23:35:49 -0500

    cambio

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 
-void main() async{ 
-   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() => runApp(MyApp());
 
-  runApp( MyApp());
-}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
